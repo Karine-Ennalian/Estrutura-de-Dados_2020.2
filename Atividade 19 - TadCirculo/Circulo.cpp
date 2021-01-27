c1cefdb401bdc638434dcaeeb16367b5d7f688d2
Karine-Ennalian <37192366+Karine-Ennalian@users.noreply.github.com>
2021-01-27 12:17:21 -0300

    Add files via upload

---
+++ Atividade 19 - TadCirculo/Circulo.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cmath>
+#include "Circulo.h"
+using namespace std;
+
+// Declaracao do Tipo de dado exportado
+struct Circulo{
+    Ponto *p;
+    double raio;
+};
+
+// Aloca e retorna um struct Circulo
+Circulo *circ_cria(double x, double y, double raio){
+    Circulo *c = new Circulo;
+
+    if (c == nullptr) {
+      cout << "Nao foi possivel alocar memoria." << endl;
+      return 0;
+    }
+    if (c != nullptr)
+    {
+        c->p = pto_cria(x, y);
+        if(c->p == nullptr){
+            return 0;
+        }
+        c->raio = raio;
+    }
+    return c;
+}
+
+// Libera a memoria que foi alocada para o struct Circulo                               
+void circ_libera(Circulo *c){
+    pto_libera(c->p);
+    delete c;
+}
+
+// setters
+void circ_setRaio(Circulo *c, double raio){
+    c->raio = raio;
+}
+
+// setters
+void circ_setCentro(Circulo *c, Ponto *p){
+    c->p = pto_distancia() / 2;
+}
+
+// setters
+// coordenada x do centro do Circulo c
+void circ_setX(Circulo *c, double x){ 
+    pto_setX(c->p, x);
+} 
+
+// setters
+// coordenada y do centro do Circulo c
+void circ_setY(Circulo *c, double y){
+    pto_setY(c->p, y);
+}
+
+// getters
+double circ_getRaio(Circulo *c, double raio){
+    return raio = c->raio;
+}
+
+// getters
+Ponto *circ_getCentro(Circulo *c){
+    return c->p;
+}
+
+// getters
+ // coordenada x do centro do Circulo c
+double circ_getX(Circulo *c){
+    return pto_getX(c->p);
+}
+
+// getters
+ // coordenada y do centro do Circulo c
+double circ_getY(Circulo *c){
+    return pto_getY(c->p);
+}
+
+// Retorna a area do Circulo c
+double circ_getArea(Circulo *c){
+    return M_PI * (c->raio * c->raio);
+}
+
+// Recebe um Ponto p e um Circulo c e retorna:
+// true: se o ponto esta contido no circulo;
+// false: caso contrario.
+bool circ_interior(Circulo *c, Ponto *p){
+    double d = pto_distancia(c->p, p);
+    return (d < c->raio);
+}
---
+++ Atividade 19 - TadCirculo/Ponto.cpp
@@ -0,0 +1,63 @@
+// Este arquivo está parcialmente implementado.
+// Implemente o restante das operações do TAD Ponto
+
+// Arquivo Ponto.cpp
+// Implementacao do TAD Ponto
+#include <iostream>
+#include <cmath>
+#include "Ponto.h"
+using namespace std;
+
+struct Ponto {
+    double x;
+    double y;
+}; 
+
+// Aloca e retorna um ponto com coordenadas (x,y)
+Ponto *pto_cria(double x, double y){
+    Ponto *p = new Ponto;
+    if (p == nullptr) {
+      cout << "Nao foi possivel alocar memoria." << endl;
+      return 0;
+   }
+    p->x = x;
+    p->y = y;
+    return p;
+}
+
+// Libera a memoria de um ponto previamente criado
+void pto_libera(Ponto *p){
+    if (p != nullptr){
+        delete p;
+        std::cout << "ponto liberado" << std::endl;
+    }
+}
+
+// getters: retornam os valores das coordenadas de um ponto 
+double pto_getX(Ponto *p, double x){
+    return x = p->x;
+}
+
+// getters: retornam os valores das coordenadas de um ponto 
+double pto_getY(Ponto *p, double y){
+    return y = p->y;
+}
+
+
+// setters: atribuem novos valores as coordenadas de um ponto 
+void pto_setX(Ponto *p, double x){
+    p->x = x;
+}
+
+// setters: atribuem novos valores as coordenadas de um ponto
+void pto_setY(Ponto *p, double y){
+    p->y = y;
+}
+
+
+// Retorna a distancia entre dois pontos
+double pto_distancia(Ponto* p1, Ponto* p2){
+    float dx = p2->x - p1->x;
+    float dy = p2->y - p1->y;
+    return sqrt(dx*dx + dy*dy); 
+}
